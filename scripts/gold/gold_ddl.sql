082227f71d93af17ecb1e20808fa5281580d86ec
Froribert Niyonkuru <forokaze11@gmail.com>
2026-01-27 17:04:16 +1030

    Refactor SQL views for better readability

--- scripts/gold/gold_ddl.sql
+++ scripts/gold/gold_ddl.sql
@@ -27,19 +27,19 @@ GO
 CREATE VIEW gold.dim_customers AS
 SELECT 
 	ROW_NUMBER() OVER(ORDER BY ci.cst_id)	AS customer_key, -- Surrogate Key to uniquely identify customers across CRM and ERP
-	ci.cst_id							                AS customer_id,
-	ci.cst_key							              AS customer_number,
-	ci.cst_firstname					            AS first_name,
-	ci.cst_lastname						            AS last_name,
-	la.country							              AS country,
-	ci.cst_marital_status				          AS marital_status,
+	ci.cst_id							AS customer_id,
+	ci.cst_key							AS customer_number,
+	ci.cst_firstname					AS first_name,
+	ci.cst_lastname						AS last_name,
+	la.country							AS country,
+	ci.cst_marital_status				AS marital_status,
 	-- Data Integration: priority given to CRM gender, fallback to ERP
 	CASE	
 		WHEN ci.cst_gndr != 'Unknown' THEN ci.cst_gndr
 		ELSE COALESCE(ca.gender, 'Unknown')            
-	END									                AS gender,
-	ca.bdate							              AS birthdate,
-	ci.cst_create_date					        AS create_date
+	END									AS gender,
+	ca.bdate							AS birthdate,
+	ci.cst_create_date					AS create_date
 FROM silver.crm_cust_info AS ci
 LEFT JOIN silver.erp_cust_az12 AS ca 
 	ON	ci.cst_key = ca.customer_id
@@ -57,16 +57,16 @@ GO
 CREATE VIEW gold.dim_products AS
 SELECT 
 	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate Key for product tracking
-	pn.prd_id												                        AS product_id,
-  pn.prd_key												                      AS product_number,
-	pn.prd_nm												                        AS product_name,
-	pn.cat_id												                        AS category_id,
-	pc.cat													                        AS category,
-	pc.subcat												                        AS subcategory,
-	pc.maintenance											                    AS maintenance,
-	pn.prd_cost												                      AS cost,
-	pn.prd_line												                      AS product_line,
-	pn.prd_start_dt											                    AS start_date
+	pn.prd_id												AS product_id,
+	pn.prd_key												AS product_number,
+	pn.prd_nm												AS product_name,
+	pn.cat_id												AS category_id,
+	pc.cat													AS category,
+	pc.subcat												AS subcategory,
+	pc.maintenance											AS maintenance,
+	pn.prd_cost												AS cost,
+	pn.prd_line												AS product_line,
+	pn.prd_start_dt											AS start_date
 FROM silver.crm_prd_info AS pn
 LEFT JOIN silver.erp_px_cat_g1v2 AS pc
 	ON pn.cat_id = pc.cat_id
@@ -87,10 +87,10 @@ SELECT
 	cu.customer_key		AS customer_key, -- Reference to dim_customers
 	sd.sls_order_dt		AS order_date,
 	sd.sls_ship_dt		AS shipping_date,
-	sd.sls_due_dt		  AS due_date,
-	sd.sls_sales		  AS sales_amount,
+	sd.sls_due_dt		AS due_date,
+	sd.sls_sales		AS sales_amount,
 	sd.sls_quantity		AS quantity,
-	sd.sls_price		  AS price
+	sd.sls_price		AS price
 FROM silver.crm_sales_details AS sd
 -- Join with Gold Dimensions to retrieve warehouse-specific Surrogate keys
 LEFT JOIN gold.dim_products AS pr
